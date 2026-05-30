37bd98806c2ac0404676c0f83c5b02139bf33948
Girjendra <shivaji12889@gmail.com>
2026-05-30 21:31:12 +0530

    Day 101 of geekstreak

---
+++ d101_replace_with_XOR_of_adjacent.cpp
@@ -0,0 +1,40 @@
+/*
+Given an array arr[] of n integers, modify the array in-place such that each element is replaced with
+the XOR of its adjacent elements.
+
+For the first element, update arr[0] = arr[0] ^ arr[1].
+For the last element, update arr[n-1] = arr[n-2] ^ arr[n-1].
+For all other elements, update arr[i] = arr[i-1] ^ arr[i+1].
+Note: Here, a ^ b represents the XOR operation between a and b. 
+*/
+#include<iostream>
+#include <vector>
+using namespace std;
+class Solution {
+  public:
+    void replaceElements(vector<int>& arr) {
+        int n = arr.size();
+        if(n == 2) {
+            int temp = arr[0];
+            arr[0] = arr[0] ^ arr[1];
+            arr[1] = temp ^ arr[1];
+            return ;
+        }
+        
+        int f = arr[0];
+        int l = arr[n-1];
+        
+        arr[0] = arr[0] ^ arr[1];
+        arr[n-1] = arr[n-2] ^ arr[n-1];
+        
+        for(int i = 1; i < n-1; i++) {
+            int temp = arr[i];
+            if(i == n-2)
+                arr[i] = f ^ l;
+            else
+                arr[i] = f ^ arr[i+1];
+                
+            f = temp;
+        }
+    }
+};
