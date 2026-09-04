11bf707d70508fd4d5ef8b47f06fbf23a2032fce
Girjendra <shivaji12889@gmail.com>
2026-09-04 10:53:58 +0530

    Day 198 of geekstreak

---
+++ d198_bird_and_max_fruit_gathering.cpp
@@ -0,0 +1,40 @@
+/*
+Given an array arr[] representing the fruit values of trees arranged in a circle and an integer m, find the maximum total fruits the bird can collect by visiting at most m trees.
+
+Bird can start from any tree and move to a neighboring tree.
+The first and last trees are also considered neighbors.
+The bird collects the fruit value of every tree it visits.
+*/
+#include<iostream>
+#include <vector>
+using namespace std;
+
+
+// TC : O(n)
+class Solution {
+  public:
+    int maxFruits(vector<int>& arr, int m) {
+        int ans = 0;
+        for(int i = 0; i < m; i++) {
+            ans += arr[i];
+        }
+        
+        int cs = ans;
+        int i = 0, j = m, n = arr.size();
+        while(j < n) {
+            ans = max(ans, cs - arr[i] + arr[j]);
+            cs = cs - arr[i] + arr[j];
+            i++, j++;
+        }
+        
+        i = n - m;
+        j = n;
+        while(i < n - 1) {
+            ans = max(ans, cs - arr[i] + arr[j % n]);
+            cs = cs - arr[i] + arr[j % n];
+            i++, j++;
+        }
+        
+        return ans;
+    }
+};
